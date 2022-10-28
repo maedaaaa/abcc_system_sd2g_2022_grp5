4d388ef81467d3cbf89df3debd643336d2850cfa
teraokiyosirou <2101400@s.asojuku.ac.jp>
2022-10-28 12:33:59 +0900

    ログインフォーム作成(仮)
    ログインフォームを簡易的に作成

---
+++ login_form.php
@@ -0,0 +1,16 @@
+<h1>アカウントをお持ちの方</h1>
+<form action="login.php" method="post">
+<div>
+    <label>
+        Eメール:
+        <input type="text" name="mail" required>
+    </label>
+</div>
+<div>
+    <label>
+        パスワード:
+        <input type="password" name="pass" required>
+    </label>
+</div>
+<input type="submit" value="ログイン">
+</form>

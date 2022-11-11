4f4a81d55920c9c126af828403cbd18e7204e556
pqpqpqpqpqpqp <2101413@s.asojuku.ac.jp>
2022-11-11 12:35:10 +0900

    いらないとこ消しました
    確認お願いします

--- (G1-3-2)addcart.php
+++ (G1-3-2)addcart.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>addcart</title>
+    <title>login</title>
 
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
@@ -16,32 +16,11 @@
 
 <!-- header読み込み -->
 <?php include_once 'header.php'; ?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"> 
-    <link href="./css/style.css" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
+<div id="app">
         <h2>カートに追加</h2>                     
         <p>カートに商品を追加しました。</p> 
         <a href="https://www.google.com/?hl=ja"><button class="btn btn-danger" >カートを見る</button>
-
         <a href="https://www.google.com/?hl=ja"><button class="btn btn-danger">戻る</button></a>   
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-    <script src="./script/script.js"></script>
-</body>
-</html>
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>

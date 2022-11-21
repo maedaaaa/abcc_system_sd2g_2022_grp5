bb88fcde82c9bafbdb2f800a0c25ade86213d69f
pqpqpqpqpqpqp <2101413@s.asojuku.ac.jp>
2022-11-21 10:39:42 +0900

    Update (G1-6)specialedition.php
    まだ未完成です。

---
+++ (G1-6)specialedition.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tokushu</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"> 
+    <link href="./css/style.css" rel="stylesheet">
+</head>
+<body>
+    <!-- header読み込み -->
+    <?php include_once 'header.php'; ?>
+    <div class="comp">
+        <h2 style="color:rgb(188, 29, 29);">季節の特集ページ</h2>
+            <p style="margin-top:40pxpx;">秋の装いにあうシックな</p>
+            <p>腕時計をご紹介。</p>
+            <a href="(G1-3-1)itempage.php">
+                <img src="omega-seamaster-aqua-terra-150m-22010412103004-l.png"
+                 alt="商品名">
+                <p>￥80000</p></a>
+            </a>

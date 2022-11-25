48f50f654731c5f458b2eefefca21ddc19c073a6
pqpqpqpqpqpqp <2101413@s.asojuku.ac.jp>
2022-11-25 11:47:13 +0900

    xammp壊れたので確認お願いします。

--- (G1-6)specialedition.php
+++ (G1-6)specialedition.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
@@ -28,4 +29,7 @@
     <p>商品名</p>
     <p>￥</p>
 </a>
-
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
